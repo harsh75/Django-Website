3987254aa7d913ee6003820c6afe9ff446cb9c83
harsh75 <harshseth75@gmail.com>
2016-07-08 05:04:12 +0530

    Editing
    Changing the layoutof the pages

--- test1/test1/urls.py
+++ test1/test1/urls.py
@@ -1,4 +1,4 @@
-"""app1 URL Configuration
+"""test1 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.9/topics/http/urls/
@@ -13,16 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf import settings
-from django.conf.urls.static import static
-from django.conf.urls import include,url
+from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^temp/', include('temp.urls')),
     url(r'^admin/', admin.site.urls),
-    url(r'^file1/', include('file1.urls')),
-    url(r'^', include('file1.urls')),
 ]
-if settings.DEBUG:
-	urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
